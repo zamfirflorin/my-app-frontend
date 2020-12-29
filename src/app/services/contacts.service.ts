a256a4facce97521f6e1efc04756d8d283586534
zamfirflorin <zamfirflorinstelian@yahoo.com>
2020-12-29 16:02:55 +0200

    Update ContactsService and create the first CRUD method to get all the contacts

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+const baseUrl = 'http://localhost:8081/contacts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll() : Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+
 }
